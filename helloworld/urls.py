d8efd435896f1b99afde20c577f8b17cc39c230d
Kristóf Dombi <kristofdombi@kinsta.com>
2023-12-21 17:26:21 +0100

    chore(kinsta): add splash screen (#2)

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home")
 ]
